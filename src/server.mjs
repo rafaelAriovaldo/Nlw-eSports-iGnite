928b921f2bfbc78d22870f9de20231880dbf73ca
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-09-20 14:15:07 -0300

    upcode

--- src/server.mjs
+++ src/server.mjs
@@ -1,15 +1,15 @@
-import  express, { response }  from 'express';
+import express, { response } from 'express';
 
 const app = express()
 
-app.get('/ads', (req,res) =>{
+app.get('/ads', (req, res) => {
     console.log("ta rodando")
     res.json(['Acessou ads com array',
-   
-    {ad:1, name:'anuncio 1'},
-    {ad:2, name:'anuncio 2'},
-    {ad:3, name:'anuncio 3'},
-   ] )
+
+        { ad: 1, name: 'anuncio 1' },
+        { ad: 2, name: 'anuncio 2' },
+        { ad: 3, name: 'anuncio 3' },
+    ])
 })
 
 app.listen(3000)
