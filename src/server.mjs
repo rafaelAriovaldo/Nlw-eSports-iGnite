ba5d43270c290406fa53d0a1dc9cc2a214a383b7
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-09-20 14:11:48 -0300

    up code

--- src/server.mjs
+++ src/server.mjs
@@ -4,7 +4,12 @@ const app = express()
 
 app.get('/ads', (req,res) =>{
     console.log("ta rodando")
-    res.json('Acessou ads.')
+    res.json(['Acessou ads com array',
+   
+    {ad:1, name:'anuncio 1'},
+    {ad:2, name:'anuncio 2'},
+    {ad:3, name:'anuncio 3'},
+   ] )
 })
 
 app.listen(3000)
