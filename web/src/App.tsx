46a6be45e3465b851a82b4a2c7cdd0b470df5975
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-09-21 19:01:15 -0300

    update code

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,15 @@
+import './style.css';
+
 interface buttonProps{
  title: string;
 }
 
-function Button(props: buttonProps){
-return(
-<button>
- {props.title}
-</button>
-)
-}
+
+
 function App() {
   return (
   <div>
-<Button title="Send 1"/>
-<Button title="Send 2"/>
-<Button title="Send 3"/>
-<Button title="Hello word"/>
+
 
   </div>
   
