ea30948ab4e0540610ee07a19b0712d46097b104
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-09-21 20:47:58 -0300

    Update code.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,8 +2,9 @@ import './styles/main.css';
 import logoIMg from './assets/Logo.svg'
 function App() {
   return (
-    <div>
-      <img src={logoIMg} alt=''/>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoIMg} alt=""/>
+      <h1 className="text-6xl text-white font-black mt-20">Seu Duo está Aqui.</h1>
     </div>
   )
 
