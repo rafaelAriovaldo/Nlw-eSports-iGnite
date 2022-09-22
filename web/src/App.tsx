1a02e7b77e80efd0adf3c73f6a4abbc7edabe3d1
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-09-21 21:31:12 -0300

    update code images.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,6 +6,23 @@ function App() {
       <img src={logoIMg} alt=""/>
       <h1 className="text-6xl text-white font-black mt-20">
         Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">Duo</span>  está Aqui.</h1>
+        <div className="grid grid-cols-6 gap-6 mt-16"> 
+          <a href="">
+            <img src="/Game-1.png" alt="" />
+          </a>
+          <a href="">
+            <img src="/Game-2.png" alt="" />
+          </a>
+          <a href="">
+            <img src="/Game-3.png" alt="" />
+          </a><a href="">
+            <img src="/Game-4.png" alt="" />
+          </a><a href="">
+            <img src="/Game-5.png" alt="" />
+          </a><a href="">
+            <img src="/Game-6.png" alt="" />
+          </a>
+        </div>
     </div>
   )
 
