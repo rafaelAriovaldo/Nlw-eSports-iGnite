35a53773540e5fd340a1393703b7c6be6683113d
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-09-21 22:22:16 -0300

    update code css boxes.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,26 +3,65 @@ import logoIMg from './assets/Logo.svg'
 function App() {
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
-      <img src={logoIMg} alt=""/>
+      <img src={logoIMg} alt="" />
       <h1 className="text-6xl text-white font-black mt-20">
         Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">Duo</span>  está Aqui.</h1>
-        <div className="grid grid-cols-6 gap-6 mt-16"> 
-          <a href="">
-            <img src="/Game-1.png" alt="" />
-          </a>
-          <a href="">
-            <img src="/Game-2.png" alt="" />
-          </a>
-          <a href="">
-            <img src="/Game-3.png" alt="" />
-          </a><a href="">
-            <img src="/Game-4.png" alt="" />
-          </a><a href="">
-            <img src="/Game-5.png" alt="" />
-          </a><a href="">
-            <img src="/Game-6.png" alt="" />
-          </a>
-        </div>
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/Games-1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className='font-bold text-white'>League of Legends</strong><br></br>
+            <strong className='text-zinc-300 text-sm block'>4 anúncios.</strong>
+
+          </div>
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/Game-2.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className='font-bold text-white'>Dota 2</strong><br></br>
+            <strong className='text-zinc-300 text-sm block'>3 anúncios.</strong>
+
+          </div>
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/Game-3.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className='font-bold text-white'>CS</strong><br></br>
+            <strong className='text-zinc-300 text-sm block'>15 anúncios.</strong>
+
+          </div>
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/Game-4.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className='font-bold text-white'>Apex Legends</strong><br></br>
+            <strong className='text-zinc-300 text-sm block'>6 anúncios.</strong>
+
+          </div>
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/Game-5.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className='font-bold text-white'>Fortnite</strong><br></br>
+            <strong className='text-zinc-300 text-sm block'>7 anúncios.</strong>
+
+          </div>
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/Game-6.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className='font-bold text-white'>Word Warcraft</strong><br></br>
+            <strong className='text-zinc-300 text-sm block'>2 anúncios.</strong>
+
+          </div>
+        </a>
+      </div>
     </div>
   )
 
