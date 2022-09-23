09efa8726f939240a0c7aa47ef3471283c05b8fa
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-09-22 23:38:09 -0300

    update code utils.

---
+++ mobile/src/utils/games.ts
@@ -0,0 +1,46 @@
+export const GAMES = [{
+
+    id: '1',
+
+    name: 'League of Legends',
+
+    ads: '4',
+
+    cover: require('../assets/games/game-1.png')
+},
+{
+    id: '2',
+
+    name: 'Dota 2',
+
+    ads: '3',
+
+    cover: require('../assets/games/game-2.png')
+},
+{
+    id: '3',
+
+    name: 'CS-GO',
+
+    ads: '2',
+
+    cover: require('../assets/games/game-3.png')
+},
+{
+    id: '4',
+    name: 'Apex',
+    ads: '3',
+    cover: require('../assets/Games/Game-4.png'),
+},
+{
+    id:'5',
+    name: 'Fortnite',
+    ads: '2',
+    cover: require('../assets/Games/Game-5.png')
+},
+{
+    id:'6',
+    name: 'World of Warcraft',
+    cover: require('../assets/Games/Game-6.png')
+},
+];
