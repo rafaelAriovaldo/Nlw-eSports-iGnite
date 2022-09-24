4c98f2b2151d3000b4443bbb95bffed65a7fcf2c
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-09-24 01:14:17 -0300

    update code mobile back ground.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,38 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-interface ButtonProps{
-  title: string;
-}
-function Button(props: ButtonProps){
-  return (
-  <TouchableOpacity>
-    <Text>
-      {props.title}
-    </Text>
-  </TouchableOpacity>
-  )
-}
+import { Background } from './src/components/background';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello react native</Text>
-      <Button title="send 1"/>
-      <Button title="send 2"/>
-      <Button title="send 3"/>
-      <Button title="Hello word"/>
-      <StatusBar style="auto" />
-    </View>
-    
+    <Background>
+
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
