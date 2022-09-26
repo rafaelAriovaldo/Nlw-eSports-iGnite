7a892dd840d2dcff373084ab8794cfb8566310a8
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-09-25 23:48:09 -0300

    up code.

---
+++ mobile/src/components/background/loading/index.tsx
@@ -0,0 +1,15 @@
+
+import { View, ActivityIndicator } from 'react-native';
+import { THEME } from '../../../theme';
+
+import { styles } from './styles';
+
+export function loading() {
+  return (
+    <View style={styles.container}>
+
+        <ActivityIndicator color={THEME.COLORS.PRIMARY}/>
+
+    </View>
+  );
+}
