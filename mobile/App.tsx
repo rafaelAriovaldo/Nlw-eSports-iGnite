eb97d6f50f3938f8dc6825f10a8cea6eda5504f3
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-09-26 22:12:21 -0300

    up

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,35 @@
+import { StatusBar } from 'react-native';
+
+import {useFonts,
+        Inter_400Regular,
+        Inter_600SemiBold,
+        Inter_700Bold,
+        Inter_900Black
+} from '@expo-google-fonts/inter';
+
+import { Home } from './src/screens/Home';
+
 
 import { Background } from './src/components/background';
 
+import  { loading } from './src/components/loading';
+ 
+
 export default function App() {
+ const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
   return (
     <Background>
-
+      <StatusBar
+       barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+      /> 
+      { fontsLoaded ? <Home />: </loading>};
     </Background>
   );
 }
