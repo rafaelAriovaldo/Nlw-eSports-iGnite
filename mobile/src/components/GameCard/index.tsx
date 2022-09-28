bfcedf3471ae6cac319ed70c74a79d786f6178cd
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-09-27 23:56:06 -0300

    update code img.

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,7 +1,8 @@
 
-import { TouchableOpacity,TouchableOpacityProps, ImageBackground, ImageSourcePropType } from 'react-native';
-
+import { TouchableOpacity,TouchableOpacityProps, ImageBackground, ImageSourcePropType, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { styles } from './styles';
+import { THEME } from '../../theme';
 
 export interface GameCardProps {
     id: string;
@@ -20,7 +21,21 @@ export function GameCard({ data,...rest }: Props) {
             <ImageBackground
                 style={styles.cover}
                 source={data.cover}
-            />
+            >
+            <LinearGradient
+            colors={THEME.COLORS.FOOTER}
+            style={styles.footer}
+            >
+            <Text style={styles.name}>
+                {data.name}
+            </Text>
+            <Text style={styles.ads}>
+                {data.ads} anúncios.
+            </Text>
+            
+            </LinearGradient>
+
+            </ImageBackground>
         </TouchableOpacity>
     );
 }
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -4,26 +4,24 @@ import { View, Image } from 'react-native';
 import logoImg from '../../assets/Games/Logo.png'
 import { Heading } from '../../components/background/Heading';
 import { GameCard } from '../../components/GameCard';
-import{GAMES} from '../../utils/games';
+import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image
-      source={logoImg}
-      style={styles.logo}
+        source={logoImg}
+        style={styles.logo}
       />
       <Heading
-      title="Encontre seu Duo"
-      subtitle="Selecione o game que deseja jogar..."
-       />
+        title="Encontre seu Duo"
+        subtitle="Selecione o game que deseja jogar..."
+      />
 
-       <GameCard 
-
-      data={GAMES[0]}
-
-       />
+      <GameCard
+        data={GAMES[0]}
+      />
     </View>
   );
 }
