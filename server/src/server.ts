7b9f2a5a79f81f9f8ea161b45bdc3e969535a7cb
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-10-03 09:10:25 -0300

    update routes.

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,32 @@ import express, { response } from 'express';
 
 const app = express()
 
+app.get('/games', (req, res) => {
+    console.log('ok')
+    return res.json([]);
+});
+
+
+app.post('/ads', (req, res) => {
+    return res.status(201).json([]);
+});
+
+app.get('/ads/:id/ads', (req, res) => {
+
+    const gameId = req.params.id;
+
+    return res.send(gameId)
+
+    return res.json(['Acessou ads com array',
+
+        { ad: 1, name: 'anuncio 1' },
+        { ad: 2, name: 'anuncio 2' },
+        { ad: 3, name: 'anuncio 3' },
+
+    ])
+
+});
+
 app.get('/ads', (req, res) => {
     console.log("ta rodando")
     res.json(['Acessou ads com array',
@@ -9,7 +35,7 @@ app.get('/ads', (req, res) => {
         { ad: 1, name: 'anuncio 1' },
         { ad: 2, name: 'anuncio 2' },
         { ad: 3, name: 'anuncio 3' },
-       
+
     ])
 })
 
