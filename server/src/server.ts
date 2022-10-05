fc89ee5ee328303657070503dd60a2da32103d78
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-10-05 11:21:54 -0300

    update code prisma rotes.

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,21 @@ import express, { response } from 'express';
 
 const app = express()
 
-import{PrismaClient} from'@prisma/client'
+import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient({
-    log:['query']
+    log: ['query']
 })
 
-app.get('/games',async (req, res) => {
+app.get('/games', async (req, res) => {
     console.log('ok')
     const games = await prisma.game.findMany({
-        
+        include: {
+            _count: {
+                select: {
+                    ad: true,
+                }
+            }
+        }
     })
     return res.json(games);
 });
@@ -20,25 +26,50 @@ app.post('/ads', (req, res) => {
     return res.status(201).json([]);
 });
 
-app.get('/ads/:id/ads', (req, res) => {
+app.get('/ads/:id/ads', async (req, res) => {
 
     const gameId = req.params.id;
+    const ads = await prisma.ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            UseVoiceChannel: true,
+            hoursStart: true,
+            hourEnd: true,
 
-    return res.send(gameId)
+        },
+        where: {
+            gameId,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        }
+    })
 
-    return res.json(['Acessou ads com array',
 
-        { ad: 1, name: 'anuncio 1' },
-        { ad: 2, name: 'anuncio 2' },
-        { ad: 3, name: 'anuncio 3' },
-
-    ])
+    return res.json(ads.map(ads => {
+        return {
+            ...ads,
+            weekDays: ads.weekDays.split(',')
+        }
+    }))
 
 });
 
-app.get('/ads/:id/discord', (req, res) => {
-
-    return res.json([])
+app.get('/ads/:id/discord', async (req, res) => {
+    const idId = req.params.id;
+    const ad = await prisma.ad.findUniqueOrThrow({
+        select: {
+            discord: true,
+        },
+        where: {
+            id: idId,
+        }
+    })
+    return res.json({
+        discord: ad.discord
+    })
 
 });
 
