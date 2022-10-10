e595eb8308bb8b3103f4019a5f7e95c4059f80b6
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-10-10 18:04:52 -0300

    update code.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 import './styles/main.css';
 import logoIMg from './assets/Logo.svg';
-import { MagnifyingGlassPlus } from 'phosphor-react';
 import { GameBanner } from './components/GameBanner';
+import { AdBanner } from './components/CreateAdBanner';
 function App() {
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
@@ -17,18 +17,7 @@ function App() {
         <GameBanner bannerUrl="/Game-5.png" title='Fortnite' adsCount={15}/>
         <GameBanner bannerUrl="Game-6.png" title='WoW' adsCount={7}/>
       </div>
-      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidde">
-        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
-          <div>
-            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
-            <span className='text-zinc-400'>Publique um anúncio para encontrar novos players!</span>
-          </div>
-          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-2'>
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
+      <AdBanner/>
     </div>
   )
 
