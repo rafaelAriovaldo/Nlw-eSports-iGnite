890bf7ebf935e0b0fd3089d32e4e193956b01d47
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-10-11 21:31:12 -0300

    update code api.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,39 +3,44 @@ import { useState, useEffect } from 'react';
 import logoIMg from './assets/Logo.svg';
 import { GameBanner } from './components/GameBanner';
 import { AdBanner } from './components/CreateAdBanner';
-interface Game{
+interface Game {
   id: string;
   title: string;
-  banneUrl: string;
-  _count:{};
-  ads: number;
+  bannerUrl: string;
+  _count: {
+    ad: number;
+  }
 }
 function App() {
 
-  const [games,setGames] = useState<Game[]>([]);
+  const [games, setGames] = useState<Game[]>([])
 
-useEffect(()=>{
-fetch('http://localhost:3000/games')
-.then(res => res.json())
-.then(data => {
-  setGames(data)
-})
-},[])
+  useEffect(() => {
+    fetch('http://localhost:3000/games')
+      .then(res => res.json())
+      .then(data => {
+        setGames(data)
+      })
+  }, [])
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoIMg} alt="" />
       <h1 className="text-6xl text-white font-black mt-20">
         Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">Duo</span>  Está Aqui.
       </h1>
-     
-      <div className="grid grid-cols-6 gap-6 mt-16">
 
-        <GameBanner bannerUrl="/Game-1.png" title='League of legends' adsCount={5} />
-        <GameBanner bannerUrl="/Game-2.png" title='Dota 2' adsCount={7} />
-        <GameBanner bannerUrl="/Game-3.png" title='CS GO' adsCount={9} />
-        <GameBanner bannerUrl="/Game-4.png" title='Apex Legends' adsCount={4} />
-        <GameBanner bannerUrl="/Game-5.png" title='Fortnite' adsCount={15} />
-        <GameBanner bannerUrl="Game-6.png" title='WoW' adsCount={7} />
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        {games.map(game => {
+          return (
+            <GameBanner
+            key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ad}
+               />
+          )
+        })}
+
       </div>
       <AdBanner />
     </div>
