3247b5988d127705b4cfa2a4915280c961f7c63d
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-10-24 15:56:22 -0300

    connection to the api.

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,13 +1,20 @@
-
-import { View, Image,FlatList } from 'react-native';
+import { useEffect, useState } from 'react';
+import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/Games/Logo.png'
 import { Heading } from '../../components/background/Heading';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([])
+  useEffect(() => {
+    fetch('http://192.168.10.10:3000/games')
+    .then(res => res.json())
+    .then(data => setGames(data))
+
+  }, []);
   return (
     <View style={styles.container}>
       <Image
@@ -19,18 +26,18 @@ export function Home() {
         subtitle="Selecione o game que deseja jogar..."
       />
       <FlatList
-      data={GAMES}
-      keyExtractor={item => item.id}
-      renderItem={({item}) =>(
-        <GameCard
-        data={item}
+        data={games}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <GameCard
+            data={item}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
       />
-      )}
-      showsHorizontalScrollIndicator={false}
-      horizontal
-      contentContainerStyle={styles.contentList}
-      />
-      
+
     </View>
   );
 }
