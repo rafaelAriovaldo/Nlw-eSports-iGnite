f5a3ccd21eab7608cf7288ae090a52a1a93f6586
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-10-25 15:27:11 -0300

    update code button.

--- mobile/src/components/DuoCard/index.tsx
+++ mobile/src/components/DuoCard/index.tsx
@@ -1,41 +1,55 @@
-import React from 'react';
-import { View } from 'react-native';
+
+import { Text, TouchableOpacity, View } from 'react-native';
+import { GameController } from 'phosphor-react-native';
 import { THEME } from '../../theme';
 import { DuoInfo } from '../DuoInfo';
 
 import { styles } from './styles';
-export interface DuoCardProps{
+export interface DuoCardProps {
   UseVoiceChannel: boolean,
   hourEnd: string,
   hoursStart: string,
   id: string,
   name: string,
-  weekDays:string[],
+  weekDays: string[],
   yearsPlaying: String;
 }
-interface Props{
+interface Props {
   data: DuoCardProps;
 }
-export function DuoCard({data}: Props) {
+export function DuoCard({ data }: Props) {
   return (
     <View style={styles.container}>
       <DuoInfo
-      label="Nome:"
-      value={data.name}
+        label="Nome:"
+        value={data.name}
       />
       <DuoInfo
-      label="Tempo de Jogo:"
-      value={`${data.yearsPlaying} anos.`}
+        label="Tempo de Jogo:"
+        value={`${data.yearsPlaying} anos.`}
       />
       <DuoInfo
-      label="Disponibilidade:"
-      value={`${data.weekDays.length} dias\u2022 ${data.hoursStart} - ${data.hourEnd}` }
+        label="Disponibilidade:"
+        value={`${data.weekDays.length} dias\u2022 ${data.hoursStart} - ${data.hourEnd}`}
       />
       <DuoInfo
-      label="Chamada de áudio?"
-      value={data.UseVoiceChannel ? "Sim" : "Não"}
-      colorValue={data.UseVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}
+        label="Chamada de áudio?"
+        value={data.UseVoiceChannel ? "Sim" : "Não"}
+        colorValue={data.UseVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}
       />
+      <TouchableOpacity
+        style={styles.button}
+        >
+        <GameController 
+          color={THEME.COLORS.TEXT}
+          size={20}
+        />
+
+
+        <Text style={styles.buttonTitle}>
+          Conectar
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
